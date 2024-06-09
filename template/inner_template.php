277de006747291143315161c254818abf2ec5827
Pavel Kuznetsov <colorofmagic@mail.ru>
2024-06-09 13:50:01 +0500

    Добавление шаблона

---
+++ template/inner_template.php
@@ -0,0 +1,8 @@
+<?php require_once ('../template/head-inner.php') ?>
+
+<body>
+  <div class="over-wrapper">
+    <?php require_once ('../template/header-inner.php') ?>
+    <!-- main -->
+
+    <?php require_once ('../template/footer-inner.php') ?>
