85292be77494ba7dee718573fa5be9cd26646ce8
Pavel Kuznetsov <colorofmagic@mail.ru>
2024-06-10 12:00:49 +0500

    gostinitsa-sadko

---
+++ hotels/sections/header/gostinitsa-sadko.php
@@ -0,0 +1,90 @@
+<div class="page-header">
+          <div class="container">
+            <ul class="breadcrumb" itemprop="http://schema.org/breadcrumb" itemscope=""
+              itemtype="http://schema.org/BreadcrumbList">
+              <li id="bx_breadcrumb_0" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                <a href="/" title="Главная" itemprop="url">
+                  Главная
+                </a>
+                <meta itemprop="position" content="1">
+              </li>
+              <li id="bx_breadcrumb_1" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                <a href="/hotels/" title="Где остановиться" itemprop="url">
+                  Где остановиться
+                </a>
+                <meta itemprop="position" content="2">
+              </li>
+              <li itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                Гостиница «Садко»
+                <meta itemprop="position" content="3">
+              </li>
+            </ul>
+            <div class="row">
+              <div class="col-xs-12 col-sm-8 col-md-7 col-lg-6">
+                <div class="page-header-content">
+                  <div class="page-header-content-inner">
+                    <div class="page-header-top">
+                      <div class="page-header-obj-1"></div>
+                      <div class="page-header-obj-2"></div>
+                      <div class="page-header-obj-3"></div>
+                      <div class="page-header-obj-4"></div>
+                      <div class="page-header-top-valign">
+                        <div class="page-header-top-valign-cont">
+                          <ul class="page-header-tags">
+                            <li><a href="/cities/velikiy-novgorod/">Великий Новгород</a></li>
+                          </ul>
+                          <h1>Гостиница «Садко»</h1>
+                          <div class="page-header-lead">
+                            <p>Комфортабельная гостиница в шаговой доступности от исторического центра города. </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="page-header-info">
+                      <div class="page-header-info-item">
+                        <div class="ico ico-type-hotel"></div>
+                        <div class="text">Тип:: <span>Гостиница</span></div>
+                      </div>
+                      <div class="page-header-info-item">
+                        <div class="page-header-rating">
+                          <div class="page-header-rating-stars">
+                            <div class="star"></div>
+                            <div class="star"></div>
+                            <div class="star"></div>
+                            <div class="star star-empty"></div>
+                            <div class="star star-empty"></div>
+                          </div>
+                          <div class="page-header-rating-ttl">Кол-во звезд:: <span>3</span></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="page-header-bottom page-header-bottom-object">
+                      <div class="object-info">
+                        <div class="object-info-item">
+                          <div class="ico ico-hotel-price"></div>
+                          <div class="cont">
+                            <div class="ttl">Стоимость<br>в сутки:</div>
+                            <div class="val">от <span>2�&nbsp;200</span> р.</div>
+                          </div>
+                        </div>
+                        <div class="object-info-item">
+
+
+                          <div class="ico ico-object-discount"></div>
+                          <div class="cont">
+                            <div class="ttl">скидка по�&nbsp;карте гостя:</div>
+                            <div class="val"><span>-10%</span></div>
+                          </div>
+
+
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="header-bottom-bg" style="background-image:url(images/b1ee3f095fbfc8aa5d013d8188ddebd4.jpg);"></div>
