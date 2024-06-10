f50d2e241f61dcbce00bf30c31523d7f3cce5f45
Pavel Kuznetsov <colorofmagic@mail.ru>
2024-06-10 11:38:07 +0500

    butik-otel-truvorbutik-otel-truvor

---
+++ hotels/butik-otel-truvor/index.php
@@ -0,0 +1,236 @@
+<?php require_once ('../template/head-inner.php') ?>
+
+<body>
+  <div class="over-wrapper">
+    <?php require_once ('../template/header-inner.php') ?>
+    <!-- main -->
+    <div class="main">
+      <div class="page-wrapper" style="background-image: url(images/page-wrapper-bg.jpg);">
+        <div class="container">
+          <div class="page-wrapper-top">
+            <div class="row">
+              <div class="col-xs-12 col-sm-4 pull-right">
+
+                <div class="sidebar-object-info sticky-sidebar">
+                  <div class="visible-xs">
+                    <div class="btn-object-map"><span>Смотреть на карте</span></div>
+                  </div>
+                  <div class="sidebar-object-map">
+                    <div class="close"></div>
+                    <div class="object-map" id="objectMap"></div>
+                    <script>
+                      document.addEventListener("DOMContentLoaded", function (event) {
+                        ymaps.ready(function () {
+                          var myMap = new ymaps.Map('objectMap', {
+                            center: [58.520286, 31.285869],
+                            zoom: 13
+                          }, {
+
+                          }),
+                            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                              hintContent: '',
+                              balloonContent: ''
+                            }, {
+                              // Опции.
+                              // Необходимо указать данный тип макета.
+                              iconLayout: 'default#image',
+                              // Своё изображение иконки метки.
+                              iconImageHref: '../images/map-pin.png',
+                              // Размеры метки.
+                              iconImageSize: [43, 60],
+                              // Смещение левого верхнего угла иконки относительно
+                              // её "ножки" (точки привязки).
+                              iconImageOffset: [-21, -60]
+                            });
+                          myMap.geoObjects
+                            .add(myPlacemark);
+
+                          $(".btn-object-map").on("click", function () {
+                            setTimeout(function () {
+                              myMap.container.fitToViewport();
+                              myMap.setZoom(15);
+                            }, 350);
+                          });
+
+                          $(window).resize(function () {
+
+                            myMap.container.fitToViewport();
+
+                          });
+
+                        });
+                      });
+                    </script>
+                  </div>
+                  <div class="sidebar-object-descr">
+                    <table class="object-info-table">
+                      <tbody>
+                        <tr>
+                          <td class="name">Телефон:</td>
+                          <td class="val"><a class="no-ul" href="tel:(8162) 55-33-33, +7-952-488-88-88">(8162) 55-33-33,
+                              +7-952-488-88-88</a></td>
+                        </tr>
+                        <tr>
+                          <td class="name">Сайт:</td>
+                          <td class="val">
+                            <a href="http://truvorhotel.com" target="_blank">truvorhotel.com</a><br>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="name">E-mail:</td>
+                          <td class="val"><a class="no-ul"
+                              href="mailto:welcome@truvorhotel.com">welcome@truvorhotel.com</a></td>
+                        </tr>
+                        <tr>
+                          <td class="name">Адрес:</td>
+                          <td class="val">Великий Новгород, ул. Дворцовая, 2/1</td>
+                        </tr>
+                        <tr>
+                          <td class="name">GPS-координаты:</td>
+                          <td class="val">
+                            <a href="javascript:void(0)" data-copy="58.520286,31.285869" class="gps-block">
+                              <span> 58.520286,31.285869 </span>
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                                <path fill="currentColor" fill-rule="evenodd"
+                                  d="M12 6h3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1v-3h2v1.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 .5-.5v-5a.5.5 0 0 0-.5-.5H12V6zM1 0h8a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1a1 1 0 0 1 1-1z">
+                                </path>
+                              </svg>
+                            </a>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-8">
+                <div class="page-content">
+                  <div class="section-expandable">
+                    <div class="h2">О бутик-отеле</div>
+                    <div class="section-expandable-content">
+                      <p></p>
+                      <div>
+                        Из окон – вид на Ярославово дворище, архитектурный комплекс XII–XVII веков. Каждый номер здесь
+                        отличает уникальный тематический дизайн: винтажные аксессуары, авторские картины, hand-made
+                        детали. <br>
+                      </div>
+                      <div>
+                        <br>
+                      </div>
+                      <div>
+                        <br>
+                      </div>
+                      *Скидка по программе "Карта гостя Новгородской области" предоставляется только при бронировании
+                      непосредственно через отель.<p></p>
+                    </div>
+                  </div>
+
+                  <div class="share-links">
+                    <div class="share-links-ttl">Поделиться:</div>
+                    <div class="share-links-cont">
+                      <script src="../js/share.js"></script>
+                      <div class="ya-share2" style="margin-top:12px;" data-curtain=""
+                        data-services="messenger,vkontakte,odnoklassniki,telegram,viber,whatsapp,moimir,skype"></div>
+                    </div>
+                  </div>
+
+
+                  <div class="section-expandable">
+                    <div class="h2">Написать отзыв</div>
+                    <div class="section-expandable-content">
+                      <div class="feedback-form">
+                        <div class="feedback-form-content">
+
+                          <form id="review-form" class="review-form" action="/hotels/butik-otel-truvor/" method="post"
+                            enctype="multipart/form-data">
+                            <input type="hidden" name="sessid" id="sessid" value="38ef1dccba42aab18d22c0718834a5b2">
+                            <input type="hidden" name="AT3cHUq2jf6I2l08409pRg==" value="UihGTc1qJ+XyJX1tWEXWbg==">
+
+                            <div class="row">
+                              <div class="col-xs-12 col-sm-4">
+                                <div class="form-group">
+                                  <label for="feedback_name">ФИО</label>
+                                  <input type="text" name="name" id="feedback_name" required="">
+                                </div>
+                              </div>
+                              <div class="col-xs-12 col-sm-4">
+                                <div class="form-group">
+                                  <label for="feedback_email">E-mail</label>
+                                  <input type="text" name="email" id="feedback_email" required="">
+                                </div>
+                              </div>
+                              <div class="col-xs-12 col-sm-4">
+                                <div class="form-group">
+                                  <label for="feedback_phone">Телефон:</label>
+                                  <input class="input-phone" type="text" name="phone" id="feedback_phone" required="">
+                                </div>
+                              </div>
+                            </div>
+                            <div class="form-group">
+                              <label>Ваша оценка</label>
+                              <input type="hidden" id="review_rating" name="rating" value="none">
+                              <div id="rateit_review_rating"></div>
+                            </div>
+
+                            <div class="form-group">
+                              <label for="form_textarea_4">Отзыв</label>
+                              <textarea class="textarea-resized" name="text" id="form_textarea_4" rows="4"
+                                required=""></textarea>
+                            </div>
+                            <div class="form-group">
+                              <label for="file_reviews" class="btn btn-2">Прикрепить фотографии</label>
+                              <input type="file" id="file_reviews" name="images[]" multiple="">
+                              <span hidden="" class="file-counts">Выбрано файлов: <span
+                                  id="file_reviews_count">0</span></span>
+                            </div>
+
+
+                            <input type="checkbox" name="confirmation" value="on" checked="" style="display: none;">
+                            <script>
+                              {
+                                let form = document.querySelector('#review-form');
+                                let checkbox = form.querySelector('[name=confirmation]')
+                                checkbox.checked = false;
+                              }
+                            </script>
+                            <input type="hidden" name="z0f6/fiRisLGnKIGeikf7Q==" value="HG92CTvIfOmxSP58F6Nt7w==">
+                            <input type="hidden" name="jAdyYcDZja2gKTJUCdCpoQ==" value="RGYKsWdTr9Gf7frK1hQF/A==">
+                            <div class="form-footer">
+                              <input class="btn btn-2" type="submit" value="Отправить">
+                            </div>
+                          </form>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <script>
+                    let reviewComponentName = "kuratovru:web.form.review";
+                  </script>
+
+
+
+                  <div class="section-expandable">
+                    <div class="h2">Отзывы</div>
+                    <div class="section-expandable-content">
+
+                      <p>Для этого объекта еще нет отзывов.</p>
+
+                    </div>
+                  </div>
+
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <?php require_once ('../template/footer-inner.php') ?>
---
+++ hotels/sections/header/butik-otel-truvor.php
@@ -0,0 +1,76 @@
+<div class="page-header">
+          <div class="container">
+            <ul class="breadcrumb" itemprop="http://schema.org/breadcrumb" itemscope=""
+              itemtype="http://schema.org/BreadcrumbList">
+              <li id="bx_breadcrumb_0" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                <a href="/" title="Главная" itemprop="url">
+                  Главная
+                </a>
+                <meta itemprop="position" content="1">
+              </li>
+              <li id="bx_breadcrumb_1" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                <a href="/hotels/" title="Где остановиться" itemprop="url">
+                  Где остановиться
+                </a>
+                <meta itemprop="position" content="2">
+              </li>
+              <li itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+                Бутик-отель «Truvor»
+                <meta itemprop="position" content="3">
+              </li>
+            </ul>
+            <div class="row">
+              <div class="col-xs-12 col-sm-8 col-md-7 col-lg-6">
+                <div class="page-header-content">
+                  <div class="page-header-content-inner">
+                    <div class="page-header-top">
+                      <div class="page-header-obj-1"></div>
+                      <div class="page-header-obj-2"></div>
+                      <div class="page-header-obj-3"></div>
+                      <div class="page-header-obj-4"></div>
+                      <div class="page-header-top-valign">
+                        <div class="page-header-top-valign-cont">
+                          <ul class="page-header-tags">
+                            <li><a href="/cities/velikiy-novgorod/">Великий Новгород</a></li>
+                          </ul>
+                          <h1>Бутик-отель «Truvor»</h1>
+                          <div class="page-header-lead">
+                            <p>Современный отель, расположенный в историческом центре Великого Новгорода.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="page-header-info">
+                      <div class="page-header-info-item">
+                        <div class="ico ico-type-hotel"></div>
+                        <div class="text">Тип:: <span>Гостиница</span></div>
+                      </div>
+                      <div class="page-header-info-item">
+                      </div>
+                    </div>
+                    <div class="page-header-bottom page-header-bottom-object">
+                      <div class="object-info">
+                        <div class="object-info-item">
+                        </div>
+                        <div class="object-info-item">
+
+
+                          <div class="ico ico-object-discount"></div>
+                          <div class="cont">
+                            <div class="ttl">скидка по�&nbsp;карте гостя:</div>
+                            <div class="val"><span>-15%</span></div>
+                          </div>
+
+
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="header-bottom-bg" style="background-image:url(images/d742eadf0ff1a9d3ad750095ea05576b.jpg);"></div>
