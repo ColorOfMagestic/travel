c91e1433ca8a7a50648b8e131148063324641e55
Pavel Kuznetsov <colorofmagic@mail.ru>
2024-06-14 10:18:13 +0500

    Удаление id от битрикса

--- guides/index.php
+++ guides/index.php
@@ -17,7 +17,7 @@
                   <div class="objects-list">
                     <div class="tr">
                       <div class="object-tmb">
-                        <a href="/guides/vladychnaya-granovitaya-palata/" id="bx_3218110189_356">
+                        <a href="/guides/vladychnaya-granovitaya-palata/" >
                           <div class="object-tmb-pic"
                             style="background-image:url(images/4068fd171a44140c8ba6c2ab8ef0f92e.jpg);">
                             <div class="object-tmb-info">
@@ -35,9 +35,9 @@
 
                         <div class="object-tmb-descr" style="min-height: 185.5px;">
                           <ul class="object-tmb-tags">
-                            <li>Музеи</li>
+                            <li>博物馆</li>
                             <li>
-                              <a href="/cities/velikiy-novgorod/">Великий Новгород</a>
+                              <a href="/cities/velikiy-novgorod/">大诺夫哥罗德 </a>
                             </li>
                           </ul>
                           <a href="/guides/vladychnaya-granovitaya-palata/">
@@ -51,7 +51,7 @@
 
                       </div>
                       <div class="object-tmb">
-                        <a href="/guides/kreml/" id="bx_3218110189_19250">
+                        <a href="/guides/kreml/" >
                           <div class="object-tmb-pic"
                             style="background-image:url(images/16cc97a42a2f24d2b4f91ca56f878467.jpg);">
                           </div>
@@ -60,9 +60,9 @@
 
                         <div class="object-tmb-descr" style="min-height: 219.5px;">
                           <ul class="object-tmb-tags">
-                            <li>Местные достопримечательности</li>
+                            <li>本地景点</li>
                             <li>
-                              <a href="/cities/velikiy-novgorod/">Великий Новгород</a>
+                              <a href="/cities/velikiy-novgorod/">大诺夫哥罗德 </a>
                             </li>
                           </ul>
                           <a href="/guides/kreml/">
@@ -78,7 +78,7 @@
                     </div>
                     <div class="tr">
                       <div class="object-tmb">
-                        <a href="/guides/dom-muzey-f-m-dostoevskogo/" id="bx_3218110189_411">
+                        <a href="/guides/dom-muzey-f-m-dostoevskogo/" >
                           <div class="object-tmb-pic"
                             style="background-image:url(images/8eb82eddaa3ea05ae8332d52eaf7b096.jpg);">
                             <div class="object-tmb-info">
@@ -96,9 +96,9 @@
 
                         <div class="object-tmb-descr" style="min-height: 161.5px;">
                           <ul class="object-tmb-tags">
-                            <li>Музеи</li>
+                            <li>博物馆</li>
                             <li>
-                              <a href="/cities/staraya-russa/">Старая Русса</a>
+                              <a href="/cities/staraya-russa/">旧鲁萨</a>
                             </li>
                           </ul>
                           <a href="/guides/dom-muzey-f-m-dostoevskogo/">
@@ -112,7 +112,7 @@
 
                       </div>
                       <div class="object-tmb">
-                        <a href="/guides/khramy-i-monastyri/sofiyskiy-sobor/" id="bx_3218110189_2689">
+                        <a href="/guides/khramy-i-monastyri/sofiyskiy-sobor/" >
                           <div class="object-tmb-pic"
                             style="background-image:url(images/b5adfb9489ebe62f39c24651ab78dd74.jpg);">
                           </div>
@@ -121,9 +121,9 @@
 
                         <div class="object-tmb-descr" style="min-height: 183.5px;">
                           <ul class="object-tmb-tags">
-                            <li>Храмы и соборы</li>
+                            <li>教堂和修道院</li>
                             <li>
-                              <a href="/cities/velikiy-novgorod/">Великий Новгород</a>
+                              <a href="/cities/velikiy-novgorod/">大诺夫哥罗德 </a>
                             </li>
                           </ul>
                           <a href="/guides/khramy-i-monastyri/sofiyskiy-sobor/">
@@ -139,7 +139,7 @@
                     </div>
                     <div class="tr">
                       <div class="object-tmb">
-                        <a href="/guides/muzei/muzey-kolokolov/" id="bx_3218110189_405">
+                        <a href="/guides/muzei/muzey-kolokolov/" >
                           <div class="object-tmb-pic"
                             style="background-image:url(images/53cd92d6113f26dfd21232f31321ca41.jpg);">
                             <div class="object-tmb-info">
@@ -157,9 +157,9 @@
 
                         <div class="object-tmb-descr" style="min-height: 119.5px;">
                           <ul class="object-tmb-tags">
-                            <li>Музеи</li>
+                            <li>博物馆</li>
                             <li>
-                              <a href="/cities/valda/">Валдай</a>
+                              <a href="/cities/valda/">瓦尔代</a>
                             </li>
                           </ul>
                           <a href="/guides/muzei/muzey-kolokolov/">
@@ -173,7 +173,7 @@
 
                       </div>
                       <div class="object-tmb">
-                        <a href="/guides/muzei/muzey-zapovednik-a-v-suvorova/" id="bx_3218110189_1864">
+                        <a href="/guides/muzei/muzey-zapovednik-a-v-suvorova/" >
                           <div class="object-tmb-pic"
                             style="background-image:url(images/43d6fa9cf6170dadf271ad3d46100f33.jpg);">
                           </div>
@@ -182,9 +182,9 @@
 
                         <div class="object-tmb-descr" style="min-height: 207.5px;">
                           <ul class="object-tmb-tags">
-                            <li>Музеи</li>
+                            <li>博物馆</li>
                             <li>
-                              <a href="/cities/borovichi/">Боровичи</a>
+                              <a href="/cities/borovichi/">博罗维奇</a>
                             </li>
                           </ul>
                           <a href="/guides/muzei/muzey-zapovednik-a-v-suvorova/">
@@ -198,7 +198,7 @@
                     </div>
                     <div class="tr">
                       <div class="object-tmb">
-                        <a href="/guides/neobychnye-mesta/ryurikovo-gorodishche/" id="bx_3218110189_1607">
+                        <a href="/guides/neobychnye-mesta/ryurikovo-gorodishche/" >
                           <div class="object-tmb-pic"
                             style="background-image:url(images/e218fdadd81dc37ea881d55e4d09e40c.jpg);">
                           </div>
@@ -207,9 +207,9 @@
 
                         <div class="object-tmb-descr" style="min-height: 161.5px;">
                           <ul class="object-tmb-tags">
-                            <li>Музеи</li>
+                            <li>博物馆</li>
                             <li>
-                              <a href="/cities/velikiy-novgorod/">Великий Новгород</a>
+                              <a href="/cities/velikiy-novgorod/">大诺夫哥罗德 </a>
                             </li>
                           </ul>
                           <a href="/guides/neobychnye-mesta/ryurikovo-gorodishche/">
@@ -223,12 +223,9 @@
 
 
                       </div>
-
-                    </div>
-                    <div class="tr">
                       <div class="object-tmb">
                         <a href="/guides/khramy-i-monastyri/iverskiy-bogoroditskiy-svyatoozerskiy-monastyr/"
-                          id="bx_1454625752_23570">
+                          >
                           <div class="object-tmb-pic"
                             style="background-image:url(images/fd319c8247c71184a4cc77a544dc0d31.jpg);">
                           </div>
@@ -237,9 +234,9 @@
 
                         <div class="object-tmb-descr" style="min-height: 119.5px;">
                           <ul class="object-tmb-tags">
-                            <li>Храмы и соборы</li>
+                            <li>教堂和修道院</li>
                             <li>
-                              <a href="/cities/valda/">Валдай</a>
+                              <a href="/cities/valda/">瓦尔代</a>
                             </li>
                           </ul>
                           <a href="/guides/khramy-i-monastyri/iverskiy-bogoroditskiy-svyatoozerskiy-monastyr/">
@@ -252,9 +249,13 @@
 
 
                       </div>
+
+
+                    </div>
+                    <div class="tr">
                       <div class="object-tmb">
                         <a href="/guides/muzei/muzey-narodnogo-derevyannogo-zodchestva-vitoslavlitsy/"
-                          id="bx_3218110189_378">
+                          >
                           <div class="object-tmb-pic"
                             style="background-image:url(images/5fd656cc751b38bc8c8b412ccaa5b153.jpg);">
                             <div class="object-tmb-info">
@@ -272,9 +273,9 @@
 
                         <div class="object-tmb-descr" style="min-height: 185.5px;">
                           <ul class="object-tmb-tags">
-                            <li>Музеи</li>
+                            <li>博物馆</li>
                             <li>
-                              <a href="/cities/velikiy-novgorod/">Великий Новгород</a>
+                              <a href="/cities/velikiy-novgorod/">大诺夫哥罗德 </a>
                             </li>
                           </ul>
                           <a href="/guides/muzei/muzey-narodnogo-derevyannogo-zodchestva-vitoslavlitsy/">
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<?php  require_once('template/head.php')?>
+﻿<?php require_once ('template/head.php') ?>
 
 <body>
   <div class="over-wrapper">
@@ -41,7 +41,7 @@
         </clipPath>
       </defs>
     </svg>
-    <?php  require_once('template/header-home.php')?>
+    <?php require_once ('template/header-home.php') ?>
     <div class="main">
       <div class="page-wrapper">
         <div class="page-wrapper-top">
@@ -77,8 +77,7 @@
                       <div class="home-cities-list-wrapper">
                         <div class="cities-list home-cities-list">
                           <div class="tr"> <a class="city-tmb" href="/cities/p-demyansk/"
-                              style="background-image:url(images/6fcb76e65c6b3d9bfcb8a11d0b057da1.jpg);"
-                              id="bx_3322728009_33">
+                              style="background-image:url(images/6fcb76e65c6b3d9bfcb8a11d0b057da1.jpg);">
                               <div class="city-tmb-inner">
                                 <div class="h3"><span>杰缅斯克</span></div>
                                 <div class="city-tmb-info">
@@ -98,8 +97,7 @@
                               </div>
                             </a>
                             <a class="city-tmb" href="/cities/p-volot/"
-                              style="background-image:url(images/58f8562a86f71f4d1defa4eb12ba4e56.jpg);"
-                              id="bx_3322728009_32">
+                              style="background-image:url(images/58f8562a86f71f4d1defa4eb12ba4e56.jpg);">
                               <div class="city-tmb-inner">
                                 <div class="h3"><span>沃洛特</span></div>
                                 <div class="city-tmb-info">
@@ -120,8 +118,7 @@
                             </a>
                           </div>
                           <div class="tr"> <a class="city-tmb" href="/cities/okulovka/"
-                              style="background-image:url(images/6061b68800611ad63ca9c1e37cf71f6f.jpg);"
-                              id="bx_3322728009_28">
+                              style="background-image:url(images/6061b68800611ad63ca9c1e37cf71f6f.jpg);">
                               <div class="city-tmb-inner">
                                 <div class="h3"><span>奥库洛夫卡</span></div>
                                 <div class="city-tmb-info">
@@ -141,8 +138,7 @@
                               </div>
                             </a>
                             <a class="city-tmb" href="/cities/chudovo/"
-                              style="background-image:url(images/3a8b84a1d226903998a2fb312f4650fd.jpg);"
-                              id="bx_3322728009_26">
+                              style="background-image:url(images/3a8b84a1d226903998a2fb312f4650fd.jpg);">
                               <div class="city-tmb-inner">
                                 <div class="h3"><span>丘多沃</span></div>
                                 <div class="city-tmb-info">
@@ -163,8 +159,7 @@
                             </a>
                           </div>
                           <div class="tr"> <a class="city-tmb" href="/cities/staraya-russa/"
-                              style="background-image:url(images/0cf0f4a3c96d0e32f1be13caa3fe4cf1.jpg);"
-                              id="bx_3322728009_4">
+                              style="background-image:url(images/0cf0f4a3c96d0e32f1be13caa3fe4cf1.jpg);">
                               <div class="city-tmb-inner">
                                 <div class="h3"><span>旧鲁萨</span></div>
                                 <div class="city-tmb-info">
@@ -184,8 +179,7 @@
                               </div>
                             </a>
                             <a class="city-tmb" href="/cities/velikiy-novgorod/"
-                              style="background-image:url(images/9fff07851708157d2bfc2464090f5be5.jpg);"
-                              id="bx_3322728009_3">
+                              style="background-image:url(images/9fff07851708157d2bfc2464090f5be5.jpg);">
                               <div class="city-tmb-inner">
                                 <div class="h3"><span>大诺夫哥罗德</span></div>
                                 <div class="city-tmb-info">
@@ -256,7 +250,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_8"><span>博物馆</span></div>
+                              <div class="h3" <span>博物馆</span></div>
                             </div>
                           </a>
 
@@ -268,7 +262,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_116"><span>教堂和修道院</span></div>
+                              <div class="h3" <span>教堂和修道院</span></div>
                             </div>
                           </a>
                         </div>
@@ -281,7 +275,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_14"><span>大自然</span></div>
+                              <div class="h3" <span>大自然</span></div>
                             </div>
                           </a>
 
@@ -293,7 +287,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_6"><span>儿童假期</span></div>
+                              <div class="h3" <span>儿童假期</span></div>
                             </div>
                           </a>
                         </div>
@@ -306,7 +300,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_124"><span>建筑物</span></div>
+                              <div class="h3" <span>建筑物</span></div>
                             </div>
                           </a>
 
@@ -318,7 +312,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_121"><span>壁画</span></div>
+                              <div class="h3" <span>壁画</span></div>
                             </div>
                           </a>
                         </div>
@@ -331,7 +325,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_9"><span>不寻常的地方</span></div>
+                              <div class="h3" <span>不寻常的地方</span></div>
                             </div>
                           </a>
 
@@ -343,7 +337,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_10"><span>健康旅游</span></div>
+                              <div class="h3" <span>健康旅游</span></div>
                             </div>
                           </a>
                         </div>
@@ -356,7 +350,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_2"><span>朝圣旅游</span></div>
+                              <div class="h3" <span>朝圣旅游</span></div>
                             </div>
                           </a>
 
@@ -368,7 +362,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_120"><span>雕塑和半身像</span></div>
+                              <div class="h3" <span>雕塑和半身像</span></div>
                             </div>
                           </a>
                         </div>
@@ -381,7 +375,7 @@
                               <div class="guide-tmb-count-ttl">个地点</div>
                             </div>
                             <div class="guide-tmb-inner">
-                              <div class="h3" id="bx_1847241719_122"><span>街道</span></div>
+                              <div class="h3" <span>街道</span></div>
                             </div>
                           </a>
 
@@ -424,7 +418,7 @@
                           <div class="tr">
                             <a class="route-tmb" href="/routes/"
                               style="background-image:url(images/34fbe6573256e2154875bfe980df8224.jpg);"
-                              id="bx_719294866_253">
+                              >
                               <div class="route-tmb-count">
                                 <div class="route-tmb-count-item">
                                   <div class="route-tmb-count-num">51</div>
@@ -445,7 +439,7 @@
 
                             <a class="route-tmb route-tmb-wide" href="/routes/"
                               style="background-image:url(images/b748fe82840103c57b884254cf681c3e.jpg);"
-                              id="bx_719294866_252">
+                              >
                               <div class="route-tmb-count">
                                 <div class="route-tmb-count-item">
                                   <div class="route-tmb-count-num">155</div>
@@ -467,7 +461,7 @@
                           <div class="tr">
                             <a class="route-tmb route-tmb-wide" href="/routes/"
                               style="background-image:url(images/c5b10d0091851bddbafeb52cb77f1a47.jpg);"
-                              id="bx_719294866_251">
+                              >
                               <div class="route-tmb-count">
                                 <div class="route-tmb-count-item">
                                   <div class="route-tmb-count-num">15</div>
@@ -488,7 +482,7 @@
 
                             <a class="route-tmb" href="/routes/"
                               style="background-image:url(images/f891577476e3f42e8b582397200ccd6f.jpg);"
-                              id="bx_719294866_250">
+                              >
                               <div class="route-tmb-count">
                                 <div class="route-tmb-count-item">
                                   <div class="route-tmb-count-num">10</div>
@@ -510,7 +504,7 @@
                           <div class="tr">
                             <a class="route-tmb" href="/routes/"
                               style="background-image:url(images/e95b0262259f1ab6ff6fba5708835de9.jpg);"
-                              id="bx_719294866_249">
+                              >
                               <div class="route-tmb-count">
                                 <div class="route-tmb-count-item">
                                   <div class="route-tmb-count-num">15</div>
@@ -531,7 +525,7 @@
 
                             <a class="route-tmb route-tmb-wide" href="/routes/"
                               style="background-image:url(images/3a81b9aa99ec42d3ea7eb21e2b28c08d.jpg);"
-                              id="bx_719294866_248">
+                              >
                               <div class="route-tmb-count">
                                 <div class="route-tmb-count-item">
                                   <div class="route-tmb-count-num">10</div>
@@ -593,7 +587,7 @@
                       </div>
                       <div class="col-xs-12 col-sm-8">
                         <div class="card-places-list card-places-list-home">
-                          <div class="card-place-tmb" id="bx_1574478084_305">
+                          <div class="card-place-tmb">
                             <div class="card-place-tmb-pic">
                               <img src="images/7ac93a963d2528ba15a81243b1aca2e2.png">
                             </div>
@@ -602,7 +596,7 @@
                               <p>圣彼得堡 — 大诺夫哥罗德 — 圣彼得堡</p>
                             </div>
                           </div>
-                          <div class="card-place-tmb" id="bx_1574478084_304">
+                          <div class="card-place-tmb">
                             <div class="card-place-tmb-pic">
                               <img src="images/0379515758a9d0f65881bb268389e2d6.png">
                             </div>
@@ -611,7 +605,7 @@
                               <p>在创意工作室和文化中心</p>
                             </div>
                           </div>
-                          <div class="card-place-tmb" id="bx_1574478084_303">
+                          <div class="card-place-tmb">
                             <div class="card-place-tmb-pic">
                               <img src="images/b702b59f2c24c89bbadc2efd72d547c9.png">
                             </div>
@@ -620,7 +614,7 @@
                               <p>儿童娱乐中心和联系动物园</p>
                             </div>
                           </div>
-                          <div class="card-place-tmb" id="bx_1574478084_302">
+                          <div class="card-place-tmb">
                             <div class="card-place-tmb-pic">
                               <img src="images/5e60040996b88a4aaaa8eae1f99ecfda.png">
                             </div>
@@ -629,7 +623,7 @@
                               <p>30多个短途旅行：戏剧、河流、公共汽车和步行、观光和个别展览</p>
                             </div>
                           </div>
-                          <div class="card-place-tmb" id="bx_1574478084_301">
+                          <div class="card-place-tmb">
                             <div class="card-place-tmb-pic">
                               <img src="images/ffd0b0f539f9068eee4384fd82380a9a.png">
                             </div>
@@ -638,7 +632,7 @@
                               <p>音乐会、表演和电影放映</p>
                             </div>
                           </div>
-                          <div class="card-place-tmb" id="bx_1574478084_300">
+                          <div class="card-place-tmb">
                             <div class="card-place-tmb-pic">
                               <img src="images/7f51eb59286ec198a198d1f919b79971.png">
                             </div>
@@ -647,7 +641,7 @@
                               <p>酒店、餐厅、咖啡馆和纪念品商店</p>
                             </div>
                           </div>
-                          <div class="card-place-tmb" id="bx_1574478084_299">
+                          <div class="card-place-tmb">
                             <div class="card-place-tmb-pic">
                               <img src="images/03cb9195231fd71a06bc5be798576284.png">
                             </div>
@@ -749,7 +743,9 @@
                               <div class="col-xs-12 col-sm-6">
                                 <div class="home-about-descr">
                                   <p>这是一个信息和教育项目。致力于诺夫哥罗德地区的旅行。我们的活动旨在创建旅游领域的单一信息资源。它提供了关于诺夫哥罗德地区旅游机会的全面信息。</p>
-                                  <p>我们向读者介绍了20000多个景点。它们包括历史和文化遗产、自然遗迹和保护区、传统手工艺和著名的俄罗斯品牌。我们出版流行的旅游路线，新闻，信息文章，活动公告。我们讲述了该地区的特征。</p>
+                                  <p>
+                                    我们向读者介绍了20000多个景点。它们包括历史和文化遗产、自然遗迹和保护区、传统手工艺和著名的俄罗斯品牌。我们出版流行的旅游路线，新闻，信息文章，活动公告。我们讲述了该地区的特征。
+                                  </p>
                                 </div>
                               </div>
                               <div class="col-xs-12 col-sm-6">
@@ -793,7 +789,8 @@
                               <div class="col-xs-12 col-sm-6">
                                 <div class="home-pros-tmb">
                                   <div class="h3">俄罗斯国家深厚历史的一部分</div>
-                                  <p>大诺夫哥罗德和诺夫哥罗德地区是一个地区。这是俄罗斯国家深厚历史的一部分。因此，在我们看来，在定位时，谈论其作为俄罗斯文化和历史中心的历史身份和意义是极其重要的。</p>
+                                  <p>大诺夫哥罗德和诺夫哥罗德地区是一个地区。这是俄罗斯国家深厚历史的一部分。因此，在我们看来，在定位时，谈论其作为俄罗斯文化和历史中心的历史身份和意义是极其重要的。
+                                  </p>
                                 </div>
                               </div>
                               <div class="col-xs-12 col-sm-6">
@@ -818,4 +815,4 @@
   </div>
 
 
-<?php  require_once('template/footer.php')?>
+  <?php require_once ('template/footer.php') ?>
