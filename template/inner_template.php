dc6e33abf086241d2dd14f01657bf635840f2f98
Pavel Kuznetsov <colorofmagic@mail.ru>
2024-06-14 11:29:57 +0500

    Перенос файла

--- template/inner_template.php
+++
@@ -1,8 +0,0 @@
-<?php require_once ('../template/head-inner.php') ?>
-
-<body>
-  <div class="over-wrapper">
-    <?php require_once ('../template/header-inner.php') ?>
-    <!-- main -->
-
-    <?php require_once ('../template/footer-inner.php') ?>
