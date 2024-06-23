4070bec07d853fc6bec48623250e16bc6ad9d5c9
Pavel Kuznetsov <colorofmagic@mail.ru>
2024-06-23 16:24:22 +0500

    Общие файлы для районов

---
+++ areas/sections/header/novgorrodskiy-raion.php
@@ -0,0 +1,54 @@
+<div class="container">
+          <ul class="breadcrumb" itemprop="http://schema.org/breadcrumb" itemscope=""
+            itemtype="http://schema.org/BreadcrumbList">
+            <li id="bx_breadcrumb_0" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+              <a href="/" title="主页" itemprop="url">
+                主页
+              </a>
+              <meta itemprop="position" content="1">
+            </li>
+            <li id="bx_breadcrumb_1" itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+              <a href="../" title="地区" itemprop="url">
+                地区
+              </a>
+              <meta itemprop="position" content="2">
+            </li>
+            <li itemprop="itemListElement" itemscope="" itemtype="http://schema.org/ListItem">
+              诺夫哥罗德区
+              <meta itemprop="position" content="3">
+            </li>
+          </ul>
+        </div>
+        <div class="page-header">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-sm-8 col-md-7 col-lg-6">
+                <div class="page-header-content">
+                  <div class="page-header-content-inner">
+                    <div class="page-header-top">
+                      <div class="page-header-obj-1">
+                      </div>
+                      <div class="page-header-obj-2">
+                      </div>
+                      <div class="page-header-obj-3">
+                      </div>
+                      <div class="page-header-obj-4">
+                      </div>
+                      <div class="page-header-top-valign">
+                        <div class="page-header-top-valign-cont">
+                          <h1>诺夫哥罗德区</h1>
+                          <div class="page-header-lead">
+                            <p></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="page-header-bottom"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="header-bottom-bg" style="background-image:url(images/16a50255d9f11998a06548efd7a8640d.jpg);"></div>
---
+++ areas/sections/titles.php
@@ -0,0 +1,14 @@
+<?
+
+$url = $_SERVER['REQUEST_URI'];
+$url = explode('?', $url);
+$url = $url[0];
+$url = substr($url, 0, strlen($url) - 1);
+$pos = strripos($url, '/');
+$name = substr($url, $pos + 1);
+
+switch ($name) {
+  case 'novgorrodskiy-raion':
+    require_once ('title/novgorrodskiy-raion.php');
+    break;
+}
